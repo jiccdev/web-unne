d422d56cd326df8db1c10038c46c0ea0368c894d
Jose Contreras <ji.ccdev@outlook.com>
2023-03-13 17:24:02 -0300

    feat: add headpage component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <div className="relative w-100 bg-white">
+      {/* HEADER PAGE */}
+
+      {/* MAIN CONTENT */}
+
+      {/* FOOTER PAGE */}
+    </div>
+  );
 };
 
 export default Layout;
