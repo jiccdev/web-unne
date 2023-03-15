896a38cf539b34aa6154855c54744070da50566c
ManuelMontenegroVerdugo <m.montenegro.verdugo@gmail.com>
2023-03-15 11:58:32 -0300

    feat:Create new folder investment

---
+++ src/components/PageSections/SoyInversionista/UnidadesRemate/AuctionProcess.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function AuctionProcess() {
+  return (
+    <div className="card w-96 bg-gray-200 shadow-lg">
+        <div className="card-body">
+          <h2 className="card-title ">
+            Transparente
+          </h2>
+          <p>La propiedad siempre está a nombre del Cliente, desde la firma de compra hasta la firma de venta</p>
+        </div>
+    </div>
+  )
+}
