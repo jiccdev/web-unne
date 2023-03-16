52ecb78abe58ae3a1dd96ccaeb7cb9fd03f029de
nicolas <nicolascabreramillacan@gmail.com>
2023-03-16 16:56:48 -0300

    feat: Circle section - responsive

--- src/pages/soy-propietario/quiero-vender.js
+++ src/pages/soy-propietario/quiero-vender.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import HeadPage from '@/components/Head/HeadPage';
+import Layout from '@/components/Layout/Layout';
+import HeroSection from '@/components/PageSections/SoyPropietario/QuieroArrendar/HeroSection';
+import CircleSection from '@/components/PageSections/SoyPropietario/QuieroArrendar/CircleSection';
+
+/* Data */
+import { CircleVenderData } from '../../data/index';
 
 const QuieroVender = () => {
-  return <div>quiero-vender</div>;
+  return (
+    <Fragment>
+      <HeadPage>
+        <title>Unne | Quiero Vender</title>
+      </HeadPage>
+
+      {/* HERO SECTION */}
+      <Layout>
+        <section>
+          seccion 1 - HeroSection
+          <HeroSection />
+        </section>
+
+        <section>
+          <CircleSection ubicationData={CircleVenderData} ColorBorder='border-amber-400' ColorBg='bg-amber-400' ColorNumberBG='bg-[#9A9BA1]' ColorTextBG='bg-[#353843]'/>
+        </section>
+      </Layout>
+    </Fragment>
+  );
 };
 
 export default QuieroVender;
