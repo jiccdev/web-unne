5a723f084bec5a552a765c1c35a7795a3001ede0
ManuelMontenegroVerdugo <m.montenegro.verdugo@gmail.com>
2023-03-16 16:44:17 -0300

    feat:Create new component Auction

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,5 +1,17 @@
 import { IoIosArrowDown } from 'react-icons/io';
+import { RiUserSearchFill } from 'react-icons/ri';
+import { BsBarChartFill } from 'react-icons/bs';
+import { GiMoneyStack } from 'react-icons/gi';
+import { TbPigMoney } from 'react-icons/tb';
+import { TbArrowBigRightFilled } from 'react-icons/tb';
+import { TbArrowBigDownFilled } from 'react-icons/tb';
 
 export const iconsList = {
   IoIosArrowDown,
+  RiUserSearchFill,
+  BsBarChartFill,
+  GiMoneyStack,
+  TbPigMoney,
+  TbArrowBigRightFilled,
+  TbArrowBigDownFilled
 };
