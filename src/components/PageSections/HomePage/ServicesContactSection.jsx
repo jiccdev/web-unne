cc39972978930a144e9d531aed718f34ff09d200
Jose Contreras <ji.ccdev@outlook.com>
2023-03-20 14:14:34 -0300

    fix: change footer buttons colors

--- src/components/PageSections/HomePage/ServicesContactSection.jsx
+++ src/components/PageSections/HomePage/ServicesContactSection.jsx
@@ -8,7 +8,7 @@ const ServicesContactSection = () => {
       <HeadingSection title="Para mas información de nuestros servicios" />
       <Button
         href="/"
-        className="w-96 p-2 text-xl xl:p-3 text-center my-6 xl:text-2xl bg-amber-400 hover:bg-amber-500"
+        className="w-96 p-2 text-xl xl:p-3 text-center my-6 xl:text-2xl bg-amber-400 hover:bg-amber-500 hover:shadow-md"
       >
         Contáctanos
       </Button>
