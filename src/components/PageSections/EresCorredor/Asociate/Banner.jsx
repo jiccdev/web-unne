c99dd760a97660a0533ff2d1939377cc3ec0cc9e
nicolas <nicolascabreramillacan@gmail.com>
2023-03-28 12:13:38 -0300

    fix: fix and change carousel in asociate.js - banner.jsx and OurPartners.jsx

--- src/components/PageSections/EresCorredor/Asociate/Banner.jsx
+++ src/components/PageSections/EresCorredor/Asociate/Banner.jsx
@@ -1,25 +1,22 @@
-import React, { Fragment, useContext, useEffect } from 'react';
-import Image from 'next/image';
-import AddCarousel from '@/components/Carousel/AddCarousel';
-import { BankData } from '../../../../data/index';
-
-import { ClientsContext } from '@/context/clients/ClientsContext';
+import React from 'react';
+import { PortalData } from '../../../../data/index';
+import ReactSlickComponent from '@/components/Carousel/ReactSlickComponent';
+import ImgCarouselCard from '@/components/Card/ImgCarouselCard';
 
 const Banner = () => {
-    const { contextData } = useContext(ClientsContext);
-    const [clients, setClients, getClientList] = contextData;
-
-    useEffect(() => {
-        getClientList();
-    }, []);
 
     return (
-        <div>
+        <div className='mb-20'>
             <div className='flex justify-center my-10 text-center'>
                 <h2 className='font-semibold text-4xl'>Publicamos en los portales mas importantes del pais</h2>
             </div>
             <div className="xl:mb-40 px-4 xl:px-32 2xl:px-48">
-                <AddCarousel data={BankData} sm={1} md={3} lg={5} xl={5} />
+                <ReactSlickComponent
+                    RenderComponent={ImgCarouselCard}
+                    data={PortalData}
+                    slidesToShow={3}
+                    xl={1}
+                />
             </div>
         </div>
     )
--- src/components/PageSections/EresCorredor/Asociate/MainInformation.jsx
+++ src/components/PageSections/EresCorredor/Asociate/MainInformation.jsx
@@ -7,6 +7,9 @@ import { AssociateData } from '../../../../data/index';
 const MainInformation = () => {
     
     return (
+        <div className='xl:mx-10 2xl:mx-48'>
+
+        
         <div className='grid grid-cols-1 xl:grid-cols-3'>
             <div className='xl:col-start-1 xl:col-end-3 py-5 px-4 sm:pl-20'>
                 {AssociateData?.length > 0 && AssociateData?.map((e) => (<Info key={e.id} data={e} />))}
@@ -16,6 +19,7 @@ const MainInformation = () => {
                     <ContactForm title="Comienza tu experiencia en Unne"></ContactForm>
                 </div>
             </div>
+        </div>
         </div>
     )
 }
