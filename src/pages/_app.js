cec31101cbb996530321dcceac919aa2e63be4fe
Jose Contreras <ji.ccdev@outlook.com>
2023-04-06 11:45:38 -0400

    feat: import mapbox styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import ClientsProvider from '@/context/clients/ClientsProvider';
 
 export default function App({ Component, pageProps }) {
