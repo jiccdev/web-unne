dd9646e5b4951eb68f69562843458b08a218bdde
ManuelMontenegroVerdugo <m.montenegro.verdugo@gmail.com>
2023-04-18 16:27:30 -0400

    feature:Fixes pages

--- src/pages/quienes-somos/index.js
+++ src/pages/quienes-somos/index.js
@@ -3,7 +3,11 @@ import HeadPage from '@/components/Head/HeadPage';
 import Layout from '@/components/Layout/Layout';
 import About from '@/components/PageSections/EresCorredor/AcademiaUnne/About';
 import ContactUs from '@/components/PageSections/EresCorredor/AcademiaUnne/ContactUs';
-
+import ReactSlickComponent from '@/components/Carousel/ReactSlickComponent';
+import { BankData } from '../../data';
+import ImgCarouselCard from '@/components/Card/ImgCarouselCard';
+import TittleAcademy from '@/components/PageSections/EresCorredor/AcademiaUnne/TittleAcademy';
+import { Fade } from 'react-awesome-reveal';
 const QuienesSomos = () => {
   return (
     <Fragment>
@@ -17,6 +21,20 @@ const QuienesSomos = () => {
         <section className="my-20 px-4 xl:px-32">
           <ContactUs />
         </section>
+
+        
+        <section className='pb-10'>
+          <Fade delay={300} direction='left'>
+            <TittleAcademy />
+            <ReactSlickComponent
+              data={BankData}
+              RenderComponent={ImgCarouselCard}
+              slidesToShow={4}
+              xl={1}
+            />
+          </Fade>
+        </section>
+
       </Layout>
     </Fragment>
   );
