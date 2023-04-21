f33fa1138ebb0450b28027689547c82eae21fd44
Jose Contreras <ji.ccdev@outlook.com>
2023-04-21 14:23:11 -0400

    feat: config next.config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-
+  reactStrictMode: true,
+  trailingSlash: true,
   images: {
     domains: [
       'reqres.in',
