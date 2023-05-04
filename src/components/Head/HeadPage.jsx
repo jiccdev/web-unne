525ff85a2df12b58b220f5933dc8c5b7b6ee78ed
Jose Contreras <ji.ccdev@outlook.com>
2023-05-04 14:22:38 -0400

    feat: add metadata

--- src/components/Head/HeadPage.jsx
+++ src/components/Head/HeadPage.jsx
@@ -5,7 +5,10 @@ const HeadPage = ({ children }) => {
   return (
     <Head>
       {children}
-      <meta name="description" content="Unne Propiedades" />
+      <meta
+        name="description"
+        content="Plataforma de corretaje, donde los corredores puedan acceder a herramientas digitales que de otro modo seria difícil de acceder"
+      />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
